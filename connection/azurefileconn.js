0530142cbf02938dde0e9f5476d3aba791bd5623
unknown <dipakdip33@.com>
2021-06-16 21:23:29 +0530

    checking file

--- connection/azurefileconn.js
+++ connection/azurefileconn.js
@@ -1,9 +1,7 @@
 const fs = require("fs")
 
 class azure {
-
     fileconnection() {
-
         const {
             ShareServiceClient,
             StorageSharedKeyCredential,
@@ -12,17 +10,15 @@ class azure {
         const account = "bsstoragedev";
         const accountKey =
             "lsaU/EdeKneKnhdxclsTZzQzZvtpd0brpCDpnMABDxuJXroIVhX9n8dc/xgGJ+oFeimythsYiItany3axV8XVg==";
-
-        // Use StorageSharedKeyCredential with storage account and account key
-        // StorageSharedKeyCredential is only available in Node.js runtime, not in browsers
         const credential = new StorageSharedKeyCredential(account, accountKey);
         const serviceClient = new ShareServiceClient(
-            // When using AnonymousCredential, following url should include a valid SAS
             `https://${account}.file.core.windows.net`,
             credential
         );
         return serviceClient;
     }
+
+
 
     async storage(hospitalname, patientId) {
         function hospital(hospitalname) {
@@ -36,29 +32,68 @@ class azure {
         var patientname = patientId;
 
         var shareName = "training";
-        var directoryName = "Max/patientABC/ECM/input";
+        var directoryName = "Hospital";
         var zipfile = "dicom.zip";
         const directoryClent = this.fileconnection();
 
 
 
         async function main() {
-            const directoryClient = conn(directoryName);
-
             function conn(directoryName) {
                 const directoryClient = directoryClent
                     .getShareClient(shareName)
                     .getDirectoryClient(directoryName);
                 return directoryClient
             }
+            const directoryClit = conn(directoryName);
 
-            const fileName = zipfile;
-            const fileClient = directoryClient.getFileClient(fileName);
-            var dirr = (process.cwd() + "/output.zip")
-            await fileClient.uploadFile(dirr, {
-                rangeSize: 4 * 1024 * 1024,
-                parallelism: 20
-            })
+
+            let dirIter = directoryClit.listFilesAndDirectories();
+            for await (const item of dirIter) {
+                if (item.name === hospitalname) {
+                    var azurepath = (directoryName + "/" + hospitalname)
+                    const directoryclnt1 = conn(azurepath)
+                    let dirIter = directoryclnt1.listFilesAndDirectories();
+                    for await (const item of dirIter) {
+                        if (item.name === patientname) {
+                            var azurepath = (directoryName + "/" + hospitalname + "/" + patientname)
+                            const directoryclnt1 = conn(azurepath)
+                            let dirIter = directoryclnt1.listFilesAndDirectories();
+                            for await (const item of dirIter) {
+                                if (item.name === "Malaria") {
+                                    var azurepath = (directoryName + "/" + hospitalname + "/" + patientname + "/" + "Malaria")
+                                    const directoryclnt1 = conn(azurepath)
+                                    const fileName = zipfile;
+                                    const fileClient = directoryclnt1.getFileClient(fileName);
+                                    var dirr = (process.cwd() + "/output.zip")
+                                    await fileClient.uploadFile(dirr, {
+                                        rangeSize: 4 * 1024 * 1024,
+                                        parallelism: 20
+                                    })
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    // const shareClient = directoryClent.getShareClient(shareName);
+                    // // var directoryname = ("directoryName" + "/" + hospitalname + "/" + patientname + "/DCN")
+                    // var directoryname = ("directoryName/same")
+                    // const directoryClient = shareClient.getDirectoryClient(directoryname);
+                    // await directoryClient.create();
+
+                    console.log("not exits")
+                }
+            }
+            // const fileName = zipfile;
+            // const fileClient = directoryClit.getFileClient(fileName);
+            // var dirr = (process.cwd() + "/output.zip")
+
+
+
+            // await fileClient.uploadFile(dirr, {
+            //     rangeSize: 4 * 1024 * 1024,
+            //     parallelism: 20
+            // })
         }
         await main();
     }
